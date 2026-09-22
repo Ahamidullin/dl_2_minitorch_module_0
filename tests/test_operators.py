4179150e39d783eaf8093cd6fb08e378515881ad
Amir <amir123tur@gmail.com>
2026-09-22 14:41:56 +0300

    Module 0: tasks + CI

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -184,7 +184,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    assert(
+    assert_close(
         minitorch.operators.sum(ls1) + minitorch.operators.sum(ls2),
         minitorch.operators.sum(addLists(ls1, ls2)),
     )
